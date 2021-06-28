b7120fbda4b2558756c47b407913179004ddd111
Tony <18810595035@163.com>
2021-06-28 17:07:43 +0800

    fixed gather_moonglass can't handle first moondial

--- scripts/modules/gather_moonglass.lua
+++ scripts/modules/gather_moonglass.lua
@@ -11,40 +11,32 @@ local function find_portal()
     end
 end
 
-local _onalterawake = nil
+local hooked = false
 ENV.AddPrefabPostInit("moondial", function(inst)
-    if not TheWorld.ismastersim then return end
-    if not _onalterawake then
-        _onalterawake = UpvalueHacker.GetUpvalue(inst.OnLoad, "onalterawake")
-        if not _onalterawake then return end
-    end
-    UpvalueHacker.SetUpvalue(inst.OnLoad, function(inst, awake, ...)
+    if hooked or not TheWorld.ismastersim then return end
+    local _onalterawake, i, prev = UpvalueHacker.GetUpvalue(inst.OnLoad, "onalterawake")
+    if not _onalterawake then return end
+    local function onalterawake_fn(inst, awake, ...)
         local portal_moonrock = find_portal()
-        if not portal_moonrock then
+        if portal_moonrock and inst.is_glassed and not awake and (POPULATING or not inst.entity:IsAwake()) then
+            inst.sg:GoToState("idle")
+            local pos = portal_moonrock:GetPosition()
+            local x, y, z = pos:Get()
+            local moonglass = TheSim:FindEntities(x, y, z, 4, {"moonglass_piece"})[1]
+            if moonglass ~= nil and not moonglass.components.stackable:IsFull() then
+                moonglass.components.stackable:SetStackSize(moonglass.components.stackable:StackSize() + 1)
+            else
+                inst.components.lootdropper:FlingItem(SpawnPrefab("moonglass"), pos)
+            end
+            inst.is_glassed = false
+        else
             return _onalterawake(inst, awake, ...)
         end
-
-        local was_glassed = inst.is_glassed
-        if not was_glassed and awake then
-            inst.is_glassed = true
-            inst.sg:GoToState((POPULATING or not inst.entity:IsAwake()) and "glassed_idle" or "glassed_pre")
-        elseif was_glassed and not awake then
-            if POPULATING or not inst.entity:IsAwake() then
-                inst.sg:GoToState("idle")
-                local pos = portal_moonrock:GetPosition()
-                local x, y, z = pos:Get()
-                local moonglass = TheSim:FindEntities(x, y, z, 4, {"moonglass_piece"} )[1]
-                if moonglass ~= nil and not moonglass.components.stackable:IsFull() then
-                    moonglass.components.stackable:SetStackSize(moonglass.components.stackable:StackSize() + 1)
-                else
-                    inst.components.lootdropper:FlingItem(SpawnPrefab("moonglass"), pos)
-                end
-                inst.is_glassed = false
-            else
-                inst.sg:GoToState("glassed_pst")
-            end
-        end
-    end, "onalterawake")
+    end
+    inst:StopWatchingWorldState("isalterawake", _onalterawake)
+    inst:WatchWorldState("isalterawake", onalterawake_fn)
+    debug.setupvalue(prev, i, onalterawake_fn)
+    hooked = true
 end)
 
 ENV.AddStategraphPostInit("moondial", function(self)
