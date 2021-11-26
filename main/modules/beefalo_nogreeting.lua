9b18febbea68e57ce262060221a838177b89153f
秋一 <civi@rcg.fun>
2021-11-27 00:14:06 +0800

    Removed.

--- main/modules/beefalo_nogreeting.lua
+++
@@ -1,8 +0,0 @@
-local AddPrefabPostInit = AddPrefabPostInit
-GLOBAL.setfenv(1, GLOBAL)
-
-AddPrefabPostInit("beefalo", function(inst)
-    if TheWorld.ismastersim then
-        inst:SetBrain(require("brains/newbeefalobrain"))
-    end
-end)
