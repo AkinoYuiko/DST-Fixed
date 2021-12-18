d4a3926b2bace5785c873c04cc1e849f3b285a81
秋一 <civi@rcg.fun>
2021-12-19 00:09:08 +0800

    Update to 2.3.4

--- modinfo.lua
+++ modinfo.lua
@@ -4,14 +4,15 @@ local boolean = {
 	{description = zh and "禁用" or "No",  data = false}
 }
 
-version = "2.3.3"
+version = "2.3.4"
 name = zh and "纯净辅助" or "DST Fixed"
 author = zh and "丁香女子学校" or "Civi, Tony"
 description = zh and "版本: "..version..[[
 
 更新内容:
-- 移除 angri_BOT 模块.
+- 提高兼容性.
 
+- 移除 angri_BOT 模块.
 - angri_BOT 不再统计玩家使用的角色.
 - 修复一个崩溃.
 - 新模块【世界相关 - 猪王给月亮碎片】.
@@ -20,8 +21,9 @@ or
 "[Version: "..version..[[]
 
 Changelog:
-- Remove angri_BOT event logs.
+- Improve compatibility with Night Stories.
 
+- Remove angri_BOT event logs.
 - angri_BOT no longer record charname.
 - Fix crash.
 - New Module (The World): Pig King reward Moon Glass during Moonstorm.
