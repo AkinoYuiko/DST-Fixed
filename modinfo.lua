e36fc0d65ab9458c92c99da4868a03f4292faf2a
秋一 <civi@rcg.fun>
2021-12-30 03:19:18 +0800

    Update desc in mod config.

--- modinfo.lua
+++ modinfo.lua
@@ -4,29 +4,30 @@ local boolean = {
     {description = zh and "禁用" or "No",  data = false}
 }
 
-version = "2.4.21"
+version = "2.4.22"
 name = zh and "纯净辅助" or "DST Fixed"
 author = zh and "丁香女子学校" or "Civi, Tony"
-description = zh and "版本: "..version..[[
-
+description = zh and "[版本: "..version..[[]
 
 更新内容:
-- 修复多处崩溃.
+- 完善MOD选项中的描述。
 
-- 新模块支持寄居蟹奶奶和称重.
-- 复活提示现在可以翻译部分来源.
-- 更新了Talker.Say的逻辑.
-- 修复风暴时间老头被󰂻的问题.
-- 【玩家相关 - 检查文本跟随客户端语言】兼容风暴事件老头的语言.
+- 修复多处崩溃。
+- 新模块支持寄居蟹奶奶和称重。
+- 复活提示现在可以翻译部分来源。
+- 更新了Talker.Say的逻辑。
+- 修复风暴事件老头被󰂻的问题。
+- 【玩家相关 - 检查文本跟随客户端语言】兼容风暴事件老头的语言。
 - <折叠了多个修复>
-- 新模块【玩家相关 - 检查文本跟随客户端语言】.
+- 新模块【玩家相关 - 检查文本跟随客户端语言】。
 ]]
 or
 "[Version: "..version..[[]
 
 Changelog:
-- Fix multi crashes.
+- Nope.
 
+- Fix multi crashes.
 - Update STRCODE_TALKER for hermit_crab.
 - RezAnnouncementString now translates most sources.
 - Update code logic for Talker.Say.
@@ -72,14 +73,14 @@ configuration_options = {
     AddTitle(zh and "玩家相关" or "The Player"),
     {
         name = "ATKSPEED",
-        label = zh and "不限制玩家攻速" or "Enable Quick Attack",
-        hover = "",
+        label = zh and "解除玩家攻速限制" or "Enable Quick Attack",
         options = boolean,
         default = false
     },
     {
         name = "SMARTUNWRAP",
-        label = zh and "拆包进物品栏" or "Inventory Unwrap",
+        label = zh and "拆包裹进物品栏" or "Inventory Unwrap",
+        hover = zh and "拆开包裹会进物品栏或箱子而不是掉落在地上",
         options = boolean,
         default = false
     },
@@ -93,7 +94,7 @@ configuration_options = {
     {
         name = "SITEMOTE",
         label = zh and "固定坐下表情" or "Static Sit Emote",
-        hover = "Static anim for /sit",
+        hover = zh and "输入/sit时一定会坐下" or "Static anim for /sit",
         options = boolean,
         default = false
     },
@@ -101,14 +102,14 @@ configuration_options = {
     {
         name = "CUSTOMFAILSTR",
         label = zh and "个性化动作失败台词" or "Custom Action Fail String",
-        hover = "",
+        hover = zh and "每个角色都会使用自己的动作失败台词而不是威尔逊的 \"我做不到。\"" or "Characters use theis own actionfail strings instead of \"I can't do that.\"",
         options = boolean,
         default = false
     },
     {
         name = "NOGHOSTHOUNDED",
         label = zh and "禁用灵魂来狗" or "Disable Ghost Hounded",
-        hover = "",
+        hover = zh and "灵魂状态的玩家在猎犬袭击开始时不会被统计进去" or "Ghost players won't be stat on hounds-attack start.",
         options = boolean,
         default = false
     },
@@ -131,7 +132,7 @@ configuration_options = {
     {
         name = "LUNARCROWN",
         label = zh and "强化启迪之冠" or "Enhanced Enlightened Crown",
-        hover = zh and "启迪之冠可放入月亮碎片" or "Moon Shard into Enlightened Crown",
+        hover = zh and "启迪之冠可放入月亮碎片代替攻击时的精神消耗\n月灵被替换为另一种特效，且可以享受玩家的倍率（不会小于1倍率）和增益加成" or "Moon Shard into Enlightened Crown",
         options = boolean,
         default = false
     },
@@ -145,27 +146,28 @@ configuration_options = {
     {
         name = "NAMEABLE_WATCHES",
         label = zh and "表可以命名" or "Nameable Backtrek Watch",
+        hover = zh and "旺达的溯源表和裂缝表可以用羽毛笔命名",
         options = boolean,
         default = false
     },
     {
         name = "NODARTWASTE",
         label = zh and "吹箭不浪费" or "No Blow Dart Waste",
-        hover = "No blow dart waste when hitting a dead target",
+        hover = zh and "吹箭打在无效目标上面时会掉落而不是消失" or "No blow dart waste when hitting a dead target",
         options = boolean,
         default = false
     },
     {
         name = "POCKETRESKIN",
         label = zh and "物品栏换皮肤" or "Reskin From Inventory",
-        hover = "",
+        hover = zh and "扫把可以对物品栏内的物品使用（手杖除外）" or "Clean Sweeper can sweep inventory items (Cane not included)",
         options = boolean,
         default = false
     },
     {
         name = "BEEQUEENHAT",
         label = zh and "禁用蜂王冠鞠躬" or "No bowing for Bee Queen Crown",
-        hover = zh and "玩家不会对其他戴着蜂王冠的玩家鞠躬" or "",
+        hover = zh and "玩家不会对其他戴着蜂王冠的玩家鞠躬",
         options = boolean,
         default = false
     },
@@ -187,14 +189,14 @@ configuration_options = {
     {
         name = "FIREFLIES",
         label = zh and "萤火虫放进蘑菇灯" or "Fireflies in lamps",
-        hover = zh and "萤火虫可以放进萤菇灯与炽菇灯(永亮)" or "Enable fireflies into lamps",
+        hover = zh and "萤火虫可以放进萤菇灯与炽菇灯实现永亮" or "Enable fireflies into lamps",
         options = boolean,
         default = false
     },
     {
         name = "SISTURN",
         label = zh and "魔改姐妹骨灰盒" or "Enhanced Sisturn",
-        hover = zh and "姐妹骨灰盒可以放骨片和哀悼荣耀" or "Allows bone shards and Mourning glory into Sisturn.",
+        hover = zh and "姐妹骨灰盒可以放骨片或哀悼荣耀" or "Allows bone shards and Mourning glory into Sisturn.",
         options = boolean,
         default = false
     },
@@ -215,36 +217,35 @@ configuration_options = {
     {
         name = "LUREPLANT",
         label = zh and "食人花无间距" or "Classic Lure Plants",
-        hover = "",
+        hover = zh and "恢复旧版食人花的放置距离为 \"无限制放置\"",
         options = boolean,
         default = false
     },
     {
         name = "ENDTABLE",
         label = zh and "漂浮的烧毁茶几" or "Floating burnt End Table",
-        hover = "",
+        hover = zh and "移除茶几烧毁动画（参考旧版本龙蝇皮肤茶几）",
         options = boolean,
         default = false
     },
     AddTitle(zh and "生物相关" or "The Mob"),
     {
         name = "BIRD",
-        label = zh and "鸟不睡觉" or "Birds no sleeping",
-        hover = zh and "鸟儿晚上不睡觉" or "Disable Birds' sleep at night.",
+        label = zh and "鸟晚上不睡觉" or "Birds no sleeping",
         options = boolean,
         default = false
     },
     {
         name = "BLOCKABLEPOOPING",
-        label = zh and "堵住牛屁股" or "Block Pooping From Beefalos",
-        hover = "",
+        label = zh and "橡胶塞堵住牛屁股" or "Block Pooping From Beefalos",
+        hover = zh and "橡胶塞可以堵住牛屁股使其不拉屎",
         options = boolean,
         default = false
     },
     {
         name = "TENTACLE",
         label = zh and "大触手无掉落物" or "Tentacle Pillars no Looting",
-        hover = zh and "大触手不再掉落任何东西(减少垃圾产生)" or "Removes Tentacle pillar's loottable",
+        hover = zh and "大触手不再掉落任何东西（减少垃圾产生）" or "Removes Tentacle pillar's loottable",
         options = boolean,
         default = false
     },
@@ -258,8 +259,8 @@ configuration_options = {
     AddTitle(zh and "世界相关" or "The World"),
     {
         name = "SEED",
-        label = zh and "种子腐烂消失" or "Seeds Disappear on Perished",
-        hover = zh and "仅普通种子(闸种)在腐烂的时候消失而不是生成腐烂食物" or "Normal seeds disappear on perished.",
+        label = zh and "普通种子腐烂后消失" or "Seeds Disappear on Perished",
+        hover = zh and "【仅】普通种子在腐烂的时候消失而不是生成腐烂食物" or "Normal seeds disappear on perished.",
         options = boolean,
         default = false
     },
@@ -273,19 +274,21 @@ configuration_options = {
     {
         name = "NOOCEANTREESTRIKEDROP",
         label = zh and "巨树被劈无掉落" or "No Ocean Tree Strike Drops",
+        hover = zh and "人工种植的巨树被雷劈后不会掉落草和树枝" or "Ocean Tress won't drop cutgrass or twigs on thunder strike.",
         options = boolean,
         default = false
     },
     {
         name = "SUMMONMAGIC",
         label = zh and "异世界召唤魔法" or "Isekai Summon Magic",
-        hover = "",
+        hover = "https://www.bilibili.com/video/BV1bo4y1S736",
         options = boolean,
         default = false
     },
     {
         name = "HONORMOUND",
-        label = zh and "哀悼荣耀填坟" or "Mourning Glory into Grave",
+        label = zh and "哀悼荣耀填坟" or "Mourning Glory into Dug Grave",
+        hover = zh and "哀悼荣耀可以恢复被挖掉的坟",
         options = boolean,
         default = false
     },
@@ -299,28 +302,28 @@ configuration_options = {
     {
         name = "GATHERMOONGLASS",
         label = zh and "月相盘在出生门掉落" or "Gather Moon Dial's Glass",
-        hover = zh and "风暴事件结束时月相盘在出生门处掉落玻璃碎片." or "Moon Glass dropped from Moon Dials will be teleported to Celestial Portal.",
+        hover = zh and "风暴事件结束时月相盘在出生门处掉落玻璃碎片" or "Moon Glass dropped from Moon Dials will be teleported to Celestial Portal.",
         options = boolean,
         default = false
     },
     {
         name = "PIGKINGMOONGLASS",
         label = zh and "猪王给月亮碎片" or "Pig King Reward Moonglass",
-        hover = zh and "月亮风暴期间猪王给月亮碎片." or "Pig King reward Moon Glass during Moonstorm.",
+        hover = zh and "月亮风暴期间猪王给月亮碎片" or "Pig King reward Moon Glass during Moonstorm.",
         options = boolean,
         default = false
     },
     {
         name = "NOFORESTRESOURCEREGEN",
         label = zh and "禁用森林资源再生" or "Disable Forest Resouces Regen",
-        hover = "",
+        hover = zh and "使草、树枝、浆果丛等不会再生",
         options = boolean,
         default = false
     },
     {
         name = "HALLOWEEN",
         label = zh and "禁用万圣节猪王糖果" or "No Halloween Candies",
-        hover = zh and "万圣节期间猪王不会给糖果(减少垃圾产生)." or "Disable halloween candies on trading with Pig King.",
+        hover = zh and "万圣节期间猪王不会给糖果（减少垃圾产生）" or "Disable halloween candies on trading with Pig King.",
         options = boolean,
         default = false
     },
@@ -328,27 +331,28 @@ configuration_options = {
     {
         name = "RANDOMLIGHTS",
         label = zh and "随机彩灯合成" or "Random Winter Lights",
-        hover = zh and "新增彩灯配方, 合成时会随机给一种款式的彩灯" or "Enable recipe for RANDOM winter lights",
+        hover = zh and "【照明栏解锁】新增彩灯配方, 合成时会随机给一种款式的彩灯" or "Enable recipe for RANDOM winter lights",
         options = boolean,
         default = false
     },
     {
         name = "CROWNFRAGMENT",
         label = zh and "启迪之冠碎片" or "Enlightened Crown Shard",
-        hover = "",
+        hover = zh and "【合成栏解锁】新增启迪之冠碎片的合成配方，无法用绿杖分解",
         options = boolean,
         default = false
     },
     {
         name = "TURFARCHIVE",
         label = zh and "档案馆地皮" or "Archive Turf",
+        hover = zh and "【夯土机制作】新增档案馆地皮配方",
         options = boolean,
         default = false
     },
     {
         name = "PROPSIGN",
         label = zh and "拍人小木牌" or "Prop Sign",
-        hover = "",
+        hover = zh and "【魔法栏解锁】新增拍人小木拍配方",
         options = boolean,
         default = false
     },
