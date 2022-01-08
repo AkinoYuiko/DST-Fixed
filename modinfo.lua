a15eae81b00b8d75ca62d8321f39f2762f72ec93
秋一 <civi@rcg.fun>
2022-01-09 06:08:22 +0800

    Fix minisign's display issue

--- modinfo.lua
+++ modinfo.lua
@@ -4,14 +4,15 @@ local boolean = {
     {description = zh and "禁用" or "No",  data = false}
 }
 
-version = "2.6.5"
+version = "2.6.6"
 name = zh and "纯净辅助" or "DST Fixed"
 author = zh and "丁香女子学校" or "Civi, Tony"
 description = zh and "[版本: "..version..[[]
 
 更新内容:
-- 优化【生物相关 - 蜂后掉落彩纸蓝图】的逻辑。
+- 修复【玩家相关 - 检查文本跟随客户端语言】关于小木牌显示名字的问题（需要重画）。
 
+- 优化【生物相关 - 蜂后掉落彩纸蓝图】的逻辑。
 - 调整部分代码格式。
 - 新模块【装备相关 - 隐藏未激活启迪之冠】。
 - 模块【装备相关 - 强化启迪之冠】不再隐藏未激活的启迪之冠，如需使用请启用新模块。
@@ -25,8 +26,8 @@ or
 "[Version: "..version..[[]
 
 Changelog:
+- Fix minisign's display issue in "Enhanced Localized Description". Needs re-drawing.
 - Tweak code logic in "Bee Queen Extra Drops".
-
 - Tweak some code format.
 - New Module: Hide Inactive Enlightened Crown.
 - Module "Enhanced Enlightened Crown" no longer hides inactive crown. Meet the new module.
