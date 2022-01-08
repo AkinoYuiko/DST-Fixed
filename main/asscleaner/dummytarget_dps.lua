d308d323ecc690f782ec6456650416cce0e22a01
秋一 <civi@rcg.fun>
2022-01-09 00:37:35 +0800

    Improve code performance.

--- main/asscleaner/dummytarget_dps.lua
+++ main/asscleaner/dummytarget_dps.lua
@@ -1,32 +1,41 @@
 local AddPrefabPostInit = AddPrefabPostInit
 GLOBAL.setfenv(1, GLOBAL)
 
-local dps_table = {}
+local function count_dps(t)
+    if t == nil then return 0 end
 
-local function count_dps(table)
-    if table == nil then return 0 end
     local dps = 0
-    for _, time, amounts in sorted_pairs(table) do
-        if GetTime() - time > 1 then
-            table[time] = nil
+    local time = GetTime() - 1
+
+    local j = 1
+    for i = 1, #t do
+        local val = t[i]
+        if val.time < time then
+            t[i] = nil
         else
-            for i = 1, #amounts do
-                dps = dps + amounts[i]
+            dps = dps + t[i].amount
+            if i ~= j then
+                t[j] = val
+                t[i] = nil
             end
+            j = j + 1
         end
     end
-    dps = math.abs(dps)
 
     return dps
 end
 
+local function record_dps(t, amount)
+    if t == nil then return end
+
+    t[#t + 1] = {amount = amount, time = GetTime()}
+end
+
 local function OnHealthDelta(inst, data)
     if data.amount <= 0 then
-        local timestamp = GetTime()
-        dps_table[timestamp] = dps_table[timestamp] or {}
-        local lenth = #dps_table[timestamp]
-        dps_table[timestamp][lenth + 1] = data.amount
-        inst.Label:SetText(data.amount .. "\nDPS: " .. count_dps(dps_table))
+        record_dps(inst.dps_history, math.abs(data.amount))
+
+        inst.Label:SetText(data.amount .. "\nDPS: " .. count_dps(inst.dps_history))
         inst.Label:SetUIOffset(math.random() * 20 - 10, math.random() * 20 + 90, 0)
         inst.AnimState:PlayAnimation("hit")
         inst.AnimState:PushAnimation("idle")
@@ -35,6 +44,8 @@ end
 
 AddPrefabPostInit("dummytarget", function(inst)
     if not TheWorld.ismastersim then return end
+
+    inst.dps_history = {}
 
     inst.event_listening["healthdelta"][inst] = nil
     inst.event_listeners["healthdelta"][inst] = nil
