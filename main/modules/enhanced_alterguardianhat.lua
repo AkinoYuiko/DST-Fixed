8b5f671a01ae606e55942e32b7c797ca8acd207d
秋一 <civi@rcg.fun>
2022-01-09 00:39:42 +0800

    No longer hide inactive crown.

--- main/modules/enhanced_alterguardianhat.lua
+++ main/modules/enhanced_alterguardianhat.lua
@@ -3,8 +3,6 @@ table.insert(PrefabFiles, "gestalt_flash")
 local AddPrefabPostInit = AddPrefabPostInit
 GLOBAL.setfenv(1, GLOBAL)
 
-local UpvalueHacker = require("tools/upvaluehacker")
-
 local function target_testfn(target, owner)
     return target and target ~= owner and target:IsValid() and
             (target.components.health == nil or not target.components.health:IsDead() and
@@ -52,15 +50,7 @@ local function alterguardianhat_test_fn(container, item, slot)
     return item:HasTag("alterguardianhatbattery") or item:HasTag("spore")
 end
 
-local function hide_on_deactive(inst, owner)
-    local sanity = owner.components.sanity ~= nil and owner.components.sanity:GetPercent() or 0
-    if sanity <= TUNING.SANITY_BECOME_ENLIGHTENED_THRESH and inst._task then
-        inst._task:Cancel()
-        inst._task = nil
-    end
-end
-
-local function new_onequip(inst, owner, ...)
+local function onequip(inst, owner, ...)
 
     local open_fn
     if inst.components.container then
@@ -75,23 +65,15 @@ local function new_onequip(inst, owner, ...)
     inst.new_spawngestalt_fn = function(_owner, _data) super_spawngestalt_fn(inst, _owner, _data) end
     inst:ListenForEvent("onattackother", inst.new_spawngestalt_fn, owner)
 
-    -- if owner.prefab == "dummy" then
-        -- inst:ListenForEvent("healthdelta", inst._onsanitydelta, owner)
-    -- end
-
     if open_fn and inst.components.container then
         inst.components.container.Open = open_fn
     end
 
-    if owner then owner.AnimState:ClearOverrideSymbol("swap_hat") end
 end
 
-local function new_onunequip(inst, owner, ...)
+local function onunequip(inst, owner, ...)
     inst.onunequip_prefns["dst-fixed"](inst, owner, ...)
     inst:RemoveEventCallback("onattackother", inst.new_spawngestalt_fn, owner)
-    -- if owner.prefab == "dummy" then
-    --     inst:RemoveEventCallback("healthdelta", inst._onsanitydeltas, owner)
-    -- end
 end
 
 local function OnEntityReplicated(inst)
@@ -125,25 +107,13 @@ AddPrefabPostInit("alterguardianhat", function(inst)
     makereadonly(container, "itemtestfn")
     makereadonly(container, "type")
 
-    local hackpath = "alterguardian_onsanitydelta.alterguardian_deactivate"
-    local alterguardian_deactivate = UpvalueHacker.GetUpvalue(inst.components.equippable.onequipfn, hackpath)
-    local function deactivate_fn(inst)
-        alterguardian_deactivate(inst)
-        if inst._task then
-            inst._task:Cancel()
-            inst._task = nil
-        end
-    end
-    UpvalueHacker.SetUpvalue(inst.components.equippable.onequipfn, hackpath, deactivate_fn)
-
-
     inst.onequip_prefns = inst.onequip_prefns or {}
     inst.onunequip_prefns = inst.onunequip_prefns or {}
     if inst.components.equippable then
         inst.onequip_prefns["dst-fixed"] = inst.components.equippable.onequipfn
         inst.onunequip_prefns["dst-fixed"] = inst.components.equippable.onunequipfn
-        inst.components.equippable:SetOnEquip(new_onequip)
-        inst.components.equippable:SetOnUnequip(new_onunequip)
+        inst.components.equippable:SetOnEquip(onequip)
+        inst.components.equippable:SetOnUnequip(onunequip)
     end
 end)
 
