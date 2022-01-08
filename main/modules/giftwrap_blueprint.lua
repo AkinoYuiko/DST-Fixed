7c5798c23ade80ba552d4a63aec15257f5bc86b4
秋一 <civi@rcg.fun>
2022-01-09 05:09:30 +0800

    small change

--- main/modules/giftwrap_blueprint.lua
+++ main/modules/giftwrap_blueprint.lua
@@ -1,4 +1,4 @@
-local AddPrefabPostInit = AddPrefabPostInit
+-- local AddPrefabPostInit = AddPrefabPostInit
 GLOBAL.setfenv(1, GLOBAL)
 
 if not IsSpecialEventActive(SPECIAL_EVENTS.WINTERS_FEAST) then
