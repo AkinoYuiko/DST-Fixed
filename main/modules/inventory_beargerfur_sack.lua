0280a043f0f0bce5380f06afee294d91d3f8ff63
Civi <civi@rcg.fun>
2023-10-27 06:24:30 +0800

    Tweak sound behavior.

--- main/modules/inventory_beargerfur_sack.lua
+++ main/modules/inventory_beargerfur_sack.lua
@@ -6,5 +6,23 @@ AddPrefabPostInit("beargerfur_sack", function(inst)
 
     if inst.components.container then
         inst.components.container.droponopen = false
+        local _onclose = inst.components.container.onclosefn
+        inst.components.container.onclosefn = function(inst)
+            _onclose(inst)
+            local owner = inst.components.inventoryitem and inst.components.inventoryitem.owner
+            if owner then
+                if owner.SoundEmitter then
+                    owner.SoundEmitter:PlaySound(inst._sounds.close)
+                end
+            end
+        end
+    end
+end)
+
+AddPrefabPostInit("tacklecontainer", function(inst)
+    if not TheWorld.ismastersim then return end
+
+    if inst.components.container then
+        inst.components.container.droponopen = false
     end
 end)
