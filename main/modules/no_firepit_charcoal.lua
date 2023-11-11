e6c15ba21064fa9ab55f99b684f7b1ac3fa99cd7
Civi <civi@rcg.fun>
2023-11-11 14:00:04 +0800

    Add Immaculate Lamb Idol

--- main/modules/no_firepit_charcoal.lua
+++ main/modules/no_firepit_charcoal.lua
@@ -1,11 +1,12 @@
 local function fn(inst)
     if not GLOBAL.TheWorld.ismastersim then return end
-    inst.disable_charcoal = false
+    inst.disable_charcoal = true
 end
 
 local firepits = {
     "firepit",
     "campfire",
+    "cotl_tabernacle_level3",
 }
 
 for _, firepit in ipairs(firepits) do
