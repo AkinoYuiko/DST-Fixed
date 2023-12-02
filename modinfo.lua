06f7bd922fa82a5cb900c62254c180e455eb8164
Civi <civi@rcg.fun>
2023-12-02 12:41:26 +0800

    2.21.4

--- modinfo.lua
+++ modinfo.lua
@@ -6,7 +6,7 @@ local function zheng(zh, en)
     return LOC[locale] or en
 end
 
-version = "2.21.3"
+version = "2.21.4"
 name = zheng("纯净辅助", "DST Fixed")
 author = zheng("鸭子乐园", "Ducklantis")
 
@@ -16,21 +16,21 @@ local new_modules = {
 
 changelog = zheng([[
 - 优化了部分代码
-
+版本更新历史：
 - 修复模块【可修复格罗姆雕像】。
 - 【火堆不掉木炭】新增完美无瑕咩咩雕像。
 - 【强化启迪之冠】调整代码逻辑。
 - 恢复模块：【固定犀牛掉落】
 ]], [[
 - Improve code performance.
-
+Version Change Notes:
 - Fix module: Glommer Statue Repairing.
 - "No Firepit Charcoal" includes Immaculate Lamb Idol.
 - Tweak code logic for module: Enhanced Enlightened Crown.
 - Revert Module: Fix Guardian's Loot
 ]])
 description = zheng("版本: ", "Version: ") .. version ..
-    zheng("\n\n更新内容:\n", "\n\nChangelog:\n") .. changelog
+    zheng("\n\n本次更新内容:\n", "\n\nChange:\n") .. changelog
 
 api_version = 10
 dst_compatible = true
