d02f46b5217ba9c40b4fe1bcdb712d0d86975630
Civi <civi@rcg.fun>
2023-12-24 14:47:47 +0800

    Update display info.

--- main/asscleaner/dummytarget_dps.lua
+++ main/asscleaner/dummytarget_dps.lua
@@ -27,18 +27,38 @@ local function record_dps(t, amount)
     t[#t + 1] = {amount = amount, time = GetTime()}
 end
 
+local function do_digits(inst)
+
+    inst.Label:SetText("Hit:" .. inst.last_hit .. "\nFrame:" .. inst._digits_num .. "\nDPS: " .. count_dps(inst.dps_history))
+    inst.Label:SetUIOffset(math.random() * 20 - 10, math.random() * 20 + 90, 0)
+    inst.AnimState:PlayAnimation("hit")
+    inst.AnimState:PushAnimation("idle")
+
+    inst._digits_task = nil
+    inst._digits_num = nil
+end
+
+
+local function pre_digits(inst, number)
+    inst.last_hit = number
+    inst._digits_num = inst._digits_num and (inst._digits_num + number) or number
+    record_dps(inst.dps_history, number)
+
+    if inst._digits_task ~= nil then
+        inst._digits_task:Cancel()
+        inst._digits_task = nil
+    end
+    inst._digits_task = inst:DoTaskInTime(0, do_digits)
+end
+
+
 local function OnHealthDelta(inst, data)
     if data.amount <= 0 then
-        record_dps(inst.dps_history, math.abs(data.amount))
-
-        inst.Label:SetText(data.amount .. "\nDPS: " .. count_dps(inst.dps_history))
-        inst.Label:SetUIOffset(math.random() * 20 - 10, math.random() * 20 + 90, 0)
-        inst.AnimState:PlayAnimation("hit")
-        inst.AnimState:PushAnimation("idle")
+        pre_digits(inst, math.abs(data.amount))
     end
 end
 
-AddPrefabPostInit("dummytarget", function(inst)
+local function dummytarget_fix(inst)
     if not TheWorld.ismastersim then return end
 
     inst.dps_history = {}
@@ -46,10 +66,25 @@ AddPrefabPostInit("dummytarget", function(inst)
     inst.event_listening["healthdelta"][inst] = nil
     inst.event_listeners["healthdelta"][inst] = nil
 
+    local health = inst.components.health
+    health:SetMaxHealth(9999 + 10)
+    health:SetMinHealth(1)
+    health:StartRegen(9999 + 10, 0.1)
+
     if inst.components.health then
         inst:ListenForEvent("healthdelta", OnHealthDelta)
     end
-end)
+end
+
+local DUMMYTARGETS =
+{
+    "dummytarget",
+    "dummytarget_lunar",
+    "dummytarget_shadow",
+}
+for _, prefab in ipairs(DUMMYTARGETS) do
+    AddPrefabPostInit(prefab, dummytarget_fix)
+end
 
 local CHS_CODES = {
     ["zh"] = true,
@@ -57,6 +92,6 @@ local CHS_CODES = {
     ["chs"] = true
 }
 
-STRINGS.NAMES.DUMMYTARGET = CHS_CODES[LanguageTranslator.defaultlang]
-                        and "伤害测试木桩"
-                        or "Dummy Target"
+STRINGS.NAMES.DUMMYTARGET = CHS_CODES[LanguageTranslator.defaultlang] and "伤害测试木桩" or "Dummy Target"
+STRINGS.NAMES.DUMMYTARGET_LUNAR = CHS_CODES[LanguageTranslator.defaultlang] and "伤害测试木桩（月亮）" or "Lunar Dummy Target"
+STRINGS.NAMES.DUMMYTARGET_SHADOW = CHS_CODES[LanguageTranslator.defaultlang] and "伤害测试木桩（暗影）" or "Shadow Dummy Target"
